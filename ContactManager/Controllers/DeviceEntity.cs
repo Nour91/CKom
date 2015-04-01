2fe3d7dd905e4b701d8d5844c0f5b10ef4033c70
Nour <n.elfaramawy@cleverkom.de>
2015-04-01 16:33:24 +0200

    ok

---
+++ ContactManager/Controllers/DeviceEntity.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Controllers
+{
+    public class DeviceEntity : TableEntity
+    {
+        public DeviceEntity(string lastName, string firstName)
+         {
+        this.PartitionKey = lastName;
+        this.RowKey = firstName;
+       }
+
+        public DeviceEntity() { }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+
+        // Retrieve the storage account from the connection string.
+CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+   CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+// Create the table client.
+CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+// Create the CloudTable object that represents the "people" table.
+CloudTable table = tableClient.GetTableReference("people");
+
+// Create a new customer entity.
+DeviceEntity customer1 = new DeviceEntity("Harp", "Walter");
+customer1.Email = "Walter@contoso.com";
+customer1.PhoneNumber = "425-555-0101";
+
+// Create the TableOperation that inserts the customer entity.
+TableOperation insertOperation = TableOperation.Insert(customer1);
+
+// Execute the insert operation.
+table.Execute(insertOperation);
+
+
+
+    }
+}
